4243a3d85286318176e3de737fb5dd0ca315c333
imsahilgupta <sahilkgupta2000@gmail.com>
2026-06-16 14:53:35 +0545

    fix: correct backend API URLs

--- client/services/auth.api.js
+++ client/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mockmate-ats.vercel.app/api/auth",
+  baseURL: "https://mockmate-backend.vercel.app/api/auth",
   withCredentials: true,
 });
 
