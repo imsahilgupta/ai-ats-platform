51f6ebe393b49bfbff398badaea9fbf28a54e190
imsahilgupta <sahilkgupta2000@gmail.com>
2026-06-17 08:02:46 +0545

    feat: initialize Express server with database connection and environment configuration

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,10 @@ connectToDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
